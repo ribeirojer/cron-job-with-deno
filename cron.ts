bf29471d08d525e48df2f554eeecb3777ad9c371
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2024-02-08 10:42:01 -0300

    add cron

---
+++ cron.ts
@@ -0,0 +1,11 @@
+const urlApiScraper = Deno.env.get("API_SCRAPER")
+
+Deno.cron("run scraper", "*/15 * * * *", () => {
+	try {
+		console.log("Cron job executado!");
+        await fetch(urlApiScraper)
+	} catch (error) {
+		console.error("Erro durante a execução do cron job:", error.message);
+	}
+});
+
