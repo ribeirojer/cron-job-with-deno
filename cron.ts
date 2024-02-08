5ea6a0ddee45bc497e61815acdd1738bb0d5ae52
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2024-02-08 10:52:46 -0300

    fix: SyntaxError

--- cron.ts
+++ cron.ts
@@ -1,9 +1,9 @@
 const urlApiScraper = Deno.env.get("API_SCRAPER")
 
-Deno.cron("run scraper", "*/15 * * * *", () => {
+Deno.cron("run scraper", "*/15 * * * *", async () => {
 	try {
 		console.log("Cron job executado!");
-        await fetch(urlApiScraper)
+        await fetch(urlApiScraper);
 	} catch (error) {
 		console.error("Erro durante a execução do cron job:", error.message);
 	}
